a0e761b1dbe49d99751f09aceba751a6d726bcd3
Nolhan <nolhan.dev@gmail.com>
2025-02-22 15:00:05 +0100

    fix: lotties on client side

--- components/homepage/GitHubIntegrationCard.vue
+++ components/homepage/GitHubIntegrationCard.vue
@@ -1,34 +1,21 @@
 <script setup lang="ts">
 import animation from "~/assets/lotties/home_github_card.json";
-import { Vue3Lottie } from "vue3-lottie";
-
-const isCardHovered = ref<boolean>(false);
 </script>
 
 <template>
-  <ClientOnly>
-    <Card
-      class="flex-[2] min-w-80 h-80 relative overflow-clip group"
-      @mouseenter="isCardHovered = true"
-      @mouseleave="isCardHovered = false"
-    >
-      <template #title>
-        <h3 class="text-center text-xl font-semibold">Made for developers</h3>
-        <p class="text-sm text-center text-balance">
-          Organize your coding projects with our GitHub integration
-        </p>
-      </template>
+  <Card class="flex-[2] min-w-80 h-80 relative overflow-clip group">
+    <template #title>
+      <h3 class="text-center text-xl font-semibold">Made for developers</h3>
+      <p class="text-sm text-center text-balance">
+        Organize your coding projects with our GitHub integration
+      </p>
+    </template>
 
-      <template #content>
-        <span
-          class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-24 w-24 group-hover:h-48 group-hover:w-48 rounded-full bg-primary blur-3xl transition-all duration-200"
-        />
-        <Vue3Lottie
-          :animation-data="animation"
-          class="max-h-64"
-          :pause-animation="!isCardHovered"
-        />
-      </template>
-    </Card>
-  </ClientOnly>
+    <template #content>
+      <span
+        class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-24 w-24 group-hover:h-48 group-hover:w-48 rounded-full bg-primary blur-3xl transition-all duration-200"
+      />
+      <ClientLottie :animation-data="animation" class="max-h-64" />
+    </template>
+  </Card>
 </template>
