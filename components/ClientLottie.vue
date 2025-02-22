d1a770fa1c657d475b50edcbaf0d46697fbb6c19
Nolhan <nolhan.dev@gmail.com>
2025-02-22 15:00:05 +0100

    fix: lotties on client side

---
+++ components/ClientLottie.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { Vue3Lottie } from "vue3-lottie";
+</script>
+
+<template>
+  <ClientOnly>
+    <Vue3Lottie v-bind="$attrs" v-on="$attrs" />
+  </ClientOnly>
+</template>
