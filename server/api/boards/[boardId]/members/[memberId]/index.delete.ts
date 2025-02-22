a7de1c798758d007d7fb399fe3774936e7f0dc24
Nolhan <nolhan.dev@gmail.com>
2025-02-22 15:05:32 +0100

    feat: created member management routes

---
+++ server/api/boards/[boardId]/members/[memberId]/index.delete.ts
@@ -0,0 +1,62 @@
+import prisma from "~/lib/prisma";
+import { z } from "zod";
+import { broadcastSSE } from "~/server/api/events/index.get";
+
+const paramsSchema = z.object({
+  boardId: z.string(),
+  memberId: z.string(),
+});
+
+export default defineEventHandler(async (event) => {
+  await requireUserSession(event, { statusCode: 401 });
+
+  const { boardId, memberId } = await getValidatedRouterParams(
+    event,
+    (params) => paramsSchema.parse(params),
+  );
+
+  const user = await auth.user(event);
+
+  if (!user)
+    throw createError({
+      statusCode: 401,
+    });
+
+  const member = await prisma.board_members.findFirst({
+    where: { parent_board: boardId, id: memberId },
+  });
+
+  if (!member)
+    throw createError({
+      statusCode: 404,
+      message: "Invitation not found",
+    });
+
+  await prisma.board_members.delete({
+    where: { parent_board: boardId, id: memberId },
+  });
+
+  broadcastSSE(
+    `boards/${boardId}`,
+    JSON.stringify({
+      type: "member_deleted",
+      data: member.id,
+    }),
+  );
+
+  return "Member deleted";
+});
+
+defineRouteMeta({
+  openAPI: {
+    tags: ["Boards"],
+    summary: "Delete member",
+    description: "Kick a member from the current board",
+    operationId: "deleteBoardMember",
+    responses: {
+      "200": {
+        description: "Member successfully deleted",
+      },
+    },
+  },
+});
---
+++ server/api/boards/[boardId]/members/[memberId]/index.patch.ts
@@ -0,0 +1,135 @@
+import prisma from "~/lib/prisma";
+import { z } from "zod";
+import { broadcastSSE } from "~/server/api/events/index.get";
+
+const paramsSchema = z.object({
+  boardId: z.string(),
+  memberId: z.string(),
+});
+
+const bodySchema = z.object({
+  role: z.enum(["reader", "member", "admin"]),
+});
+
+export default defineEventHandler(async (event) => {
+  await requireUserSession(event, { statusCode: 401 });
+
+  const { boardId, memberId } = await getValidatedRouterParams(
+    event,
+    (params) => paramsSchema.parse(params),
+  );
+
+  const body = await readValidatedBody(event, (body) => bodySchema.parse(body));
+
+  const user = await auth.user(event);
+
+  if (!user)
+    throw createError({
+      statusCode: 401,
+    });
+
+  const board = await prisma.boards.findFirst({
+    where: { id: boardId },
+    include: {
+      members: {
+        select: {
+          user_id: true,
+          role: true,
+        },
+      },
+    },
+  });
+
+  if (!board)
+    throw createError({
+      statusCode: 404,
+      message: "Board not found",
+    });
+
+  if (
+    board.owner_id !== user.id &&
+    !board.members.find(
+      (member) => member.user_id === user.id && member.role === "admin",
+    )
+  )
+    throw createError({
+      statusCode: 403,
+    });
+
+  const member = await prisma.board_members.findFirst({
+    where: { parent_board: boardId, id: memberId },
+  });
+
+  if (!member)
+    throw createError({
+      statusCode: 404,
+      message: "Invitation not found",
+    });
+
+  if (member.user_id === user.id)
+    throw createError({
+      statusCode: 403,
+    });
+
+  const newMember = await prisma.board_members.update({
+    where: { parent_board: boardId, id: memberId },
+    data: {
+      role: body.role,
+    },
+    include: {
+      users: {
+        select: {
+          full_name: true,
+          email: true,
+        },
+      },
+    },
+  });
+
+  broadcastSSE(
+    `boards/${boardId}`,
+    JSON.stringify({
+      type: "member_updated",
+      data: {
+        ...newMember,
+        users: undefined,
+        full_name: newMember.users!.full_name,
+        avatar: getGravatar(newMember.users!.email),
+        email: newMember.users!.email,
+      },
+    }),
+  );
+
+  return "Member updated";
+});
+
+defineRouteMeta({
+  openAPI: {
+    tags: ["Boards"],
+    summary: "Update member",
+    description: "Update the role associated to a member",
+    operationId: "updateBoardMember",
+    requestBody: {
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              role: {
+                type: "string",
+                enum: ["reader", "member", "admin"],
+                description: "The new role of the member",
+                example: "reader",
+              },
+            },
+          },
+        },
+      },
+    },
+    responses: {
+      "200": {
+        description: "Member successfully updated",
+      },
+    },
+  },
+});
