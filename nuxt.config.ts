c1040ec0312df4a954273dfdd853f85a86d50ffd
Nolhan <nolhan.dev@gmail.com>
2025-03-06 14:49:26 +0100

    feat: added popup to BoardCard component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,6 +19,7 @@ export default defineNuxtConfig({
     "@nuxt/image",
     "nuxt-auth-utils",
     "@prisma/nuxt",
+    "@nuxtjs/mdc",
   ],
   tailwindcss: { exposeConfig: true },
   css: ["primeicons/primeicons.css"],
@@ -91,5 +92,10 @@ export default defineNuxtConfig({
   prisma: {
     runMigration: true,
     skipPrompts: true,
+  },
+  mdc: {
+    headings: {
+      anchorLinks: false,
+    },
   },
 });
