3b3bee7f11078f2c9435b231ea01c1e37971f0f8
Nolhan <nolhan.dev@gmail.com>
2025-03-06 14:49:46 +0100

    feat: added 'workflows' card to homepage

---
+++ components/homepage/WorkflowsCard.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import animation from "~/assets/lotties/home_workflows_card.json";
+</script>
+
+<template>
+  <Card class="flex-[2] min-w-80 h-80 relative overflow-clip group">
+    <template #title>
+      <h3 class="text-center text-xl font-semibold flex justify-center">
+        Create awesome workflows<span class="font-normal text-xs">*</span>
+      </h3>
+      <p class="text-sm text-center text-balance">
+        Save time with automated workflows to increase your productivity
+      </p>
+    </template>
+
+    <template #content>
+      <span
+        class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-24 w-24 group-hover:h-48 group-hover:w-48 rounded-full bg-primary blur-3xl transition-all duration-200"
+      />
+      <ClientLottie :animation-data="animation" class="max-h-64" />
+    </template>
+  </Card>
+</template>
